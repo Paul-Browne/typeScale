0e55fa170ec20c7c926a97c2a06ef368a5204c85
paulbrowne <Paulmatthewbrowne@gmail.com>
2014-11-13 12:05:24 +0200

    Update typeScale.js

--- typeScale.js
+++ typeScale.js
@@ -4,27 +4,20 @@ function typeScale(){
     
     // http://andylangton.co.uk/blog/development/get-viewport-size-width-and-height-javascript
     
-    var a=window,
-        b=document,
-        c=b.documentElement,
-        d=b.getElementsByTagName("body")[0],
-        x=a.innerWidth||c.clientWidth||d.clientWidth;
+    var a = window,
+        b = document,
+        c = b.documentElement,
+        d = b.getElementsByTagName("body")[0],
+        x = a.innerWidth||c.clientWidth||d.clientWidth;
         
-    // The actual math part for tweeking the font-size, the pixel equivalent is
-    // g.style.fontSize = x/320 + 13.5 + "px";
+    // The actual math part for tweeking the font-size,
     // where 'x' is the viewport width
         
-    d.style.fontSize=(x+4320)/5120+"em",
+    d.style.fontSize = x/320 + 13.5 + "px",
     
     // Tweeks the line-height ever so slightly, by 0.05 every 400px of width
     
-    d.style.lineHeight=125e-6*x+1.25
+    d.style.lineHeight = x/8000 + 1.25
 }
 window.addEventListener("resize",typeScale),
 typeScale();
-
-
-
-
-
-
